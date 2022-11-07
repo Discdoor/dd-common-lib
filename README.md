4b8896abc3c0d99529dcf9dec31bf1bca5f1bf1b
ralph <ralph@localhost>
2022-11-07 23:50:21 +0100

    initial commit for common lib

---
+++ README.md
@@ -0,0 +1,3 @@
+# Doordisc common library for NodeJS projects
+
+This library contains common functions for Doordisc (schema validation, regexes, etc.) which is shared between each project.
---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+libdd-node: Doordisc NodeJS common library.
+
+Copyright (C) Doordisc 2022. Licensed under GNU GPL version 3.0.
+*/
+
+module.exports = {
+    api: require('./lib/api'),
+    schema: require('./lib/schema-validator')
+}
